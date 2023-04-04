70d3bd38794bb27626be07552c09ffb98d3fe81f
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-04-04 11:54:51 +0700

    update components Thread

--- src/components/Thread/ThreadAdd.jsx
+++ src/components/Thread/ThreadAdd.jsx
@@ -3,26 +3,17 @@ import React from 'react';
 import { XMarkIcon, PlusIcon } from '@heroicons/react/24/outline';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { useInput, useToggle } from '../../hooks';
+import { useToggle } from '../../hooks';
 import { asyncCreateThread } from '../../app/states/threads/action';
+import ThreadForm from './ThreadForm';
 
 export default function ThreadAdd() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [title, setTitle, changeTitle] = useInput('');
-  const [category, setCategory, changeCategory] = useInput('');
-  const [body, setBody, changeBody] = useInput('');
   const [checked, setChecked, changeChecked] = useToggle(false);
 
-  const onToggleChecked = () => {
-    setChecked();
-    changeTitle('');
-    changeCategory('');
-    changeBody('');
-  };
-
-  const onCreateThread = () => {
-    dispatch(asyncCreateThread({ title, body, category })).then(
+  const onCreateThread = async ({ title, body, category }) => {
+    await dispatch(asyncCreateThread({ title, body, category })).then(
       ({ status }) => {
         if (status === 'success') {
           changeChecked(false);
@@ -45,45 +36,19 @@ export default function ThreadAdd() {
         id="create-thread-modal"
         className="modal-toggle"
         checked={checked}
-        onChange={onToggleChecked}
+        onChange={setChecked}
       />
       <div className="modal modal-bottom sm:modal-middle">
         <div className="modal-box relative">
           <button
             type="button"
             className="btn btn-xs btn-circle absolute right-2 top-2"
-            onClick={onToggleChecked}
+            onClick={setChecked}
           >
             <XMarkIcon />
           </button>
           <h3 className="font-bold text-lg">Create Thread</h3>
-          <div className="flex flex-col gap-3 py-4">
-            <input
-              type="text"
-              placeholder="Title"
-              className="input input-bordered w-full"
-              value={title}
-              onChange={setTitle}
-            />
-            <input
-              type="text"
-              placeholder="Category"
-              className="input input-bordered w-full"
-              value={category}
-              onChange={setCategory}
-            />
-            <textarea
-              value={body}
-              onChange={setBody}
-              className="textarea textarea-bordered w-full h-24"
-              placeholder="Body"
-            />
-          </div>
-          <div className="modal-action justify-center">
-            <button type="button" className="btn" onClick={onCreateThread}>
-              Add Thread
-            </button>
-          </div>
+          <ThreadForm onCreateThread={onCreateThread} />
         </div>
       </div>
     </>
