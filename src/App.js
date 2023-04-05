0e5bf899f75660c65a5d4bb3b674bfa5e430b369
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-04-05 13:36:39 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import {
+  Navigate, Route, Routes, useNavigate,
+} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import LoadingBar from 'react-redux-loading-bar';
 import ThreadsPage from './pages/ThreadsPage';
@@ -9,10 +11,18 @@ import { asyncIsPreload } from './app/states/isPreload/action';
 import ThreadPage from './pages/ThreadPage';
 import LeaderboardsPage from './pages/LeaderboardsPage';
 import { Navbar } from './components';
+import { asyncLogout } from './app/states/authUser/action';
 
 function App() {
   const dispatch = useDispatch();
   const { authUser, isPreload } = useSelector((state) => state);
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    dispatch(asyncLogout()).then(({ status }) => {
+      if (status === 'success') navigate('/login');
+    });
+  };
 
   useEffect(() => {
     dispatch(asyncIsPreload());
@@ -24,14 +34,14 @@ function App() {
 
   return (
     <>
-      <Navbar />
+      <Navbar authUser={authUser} onLogout={onLogout} />
       <LoadingBar className="bg-primary h-1 absolute" />
       <main className="mx-auto max-w-7xl p-4">
         <Routes>
           <Route path="/" element={<Navigate to="/threads" />} />
           <Route path="/threads" element={<ThreadsPage />} />
           <Route path="/thread/:id" element={<ThreadPage />} />
-          <Route path="/leaderbords" element={<LeaderboardsPage />} />
+          <Route path="/leaderboards" element={<LeaderboardsPage />} />
           <Route path="/login" element={authUser ? <Navigate to="/threads" /> : <LoginPage />} />
           <Route path="/register" element={authUser ? <Navigate to="/threads" /> : <RegisterPage />} />
         </Routes>
